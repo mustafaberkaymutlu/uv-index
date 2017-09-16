00113b312b7acc810f2f724fee134d06772fd828
Mustafa Berkay Mutlu <mustafaberkaymutlu@gmail.com>
2017-09-16 14:00:03 +0300

    initial commit

---
+++ query/src/main/java/net/epictimes/uvindex/query/QueryActivity.kt
@@ -0,0 +1,12 @@
+package net.epictimes.uvindex.query
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class QueryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_query)
+    }
+}
