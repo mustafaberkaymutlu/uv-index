9e20adeada2f89ced8337a495f022629de657d36
Mustafa Berkay Mutlu <mustafaberkaymutlu@gmail.com>
2018-01-25 23:06:23 +0300

    Move FetchIntentService strings to autocomplete module

--- autocomplete/src/main/java/net/epictimes/uvindex/autocomplete/FetchAddressIntentService.kt
+++ autocomplete/src/main/java/net/epictimes/uvindex/autocomplete/FetchAddressIntentService.kt
@@ -7,7 +7,6 @@ import android.location.Address
 import android.location.Geocoder
 import android.os.Bundle
 import android.os.ResultReceiver
-import net.epictimes.uvindex.base.R
 import timber.log.Timber
 import java.io.IOException
 import java.util.*
